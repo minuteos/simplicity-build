0bee07c1ba07f0e12b6e842fa1af4a6b702b0570
Štefan Šimek <simek@triaxis.sk>
2020-03-26 13:26:29 +0100

    Gecko platform 2.7.3, BLE 2.13.3

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ export DOCKER_BUILDKIT=1
 
 docker build -f Dockerfile -t triaxis/simplicity-studio:4-base .
 docker build -f Dockerfile-gcc -t triaxis/simplicity-studio:4-gcc-7 .
-docker build -f Dockerfile-gecko -t triaxis/simplicity-studio:4-gecko-2.7.1 .
-docker build -f Dockerfile-ble -t triaxis/simplicity-studio:4-ble-2.13.1 .
+docker build -f Dockerfile-gecko -t triaxis/simplicity-studio:4-gecko-2.7.3 .
+docker build -f Dockerfile-ble -t triaxis/simplicity-studio:4-ble-2.13.3 .
