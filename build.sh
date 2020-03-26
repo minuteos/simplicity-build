3f8417920e18945b00540b567452c8624d58edb4
Štefan Šimek <simek@triaxis.sk>
2020-03-26 13:22:48 +0100

    Gecko platform 2.7.1, BLE 2.13.1, add si-build for Simplicity projects

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ export DOCKER_BUILDKIT=1
 
 docker build -f Dockerfile -t triaxis/simplicity-studio:4-base .
 docker build -f Dockerfile-gcc -t triaxis/simplicity-studio:4-gcc-7 .
-docker build -f Dockerfile-gecko -t triaxis/simplicity-studio:4-gecko-2.7.0 .
-docker build -f Dockerfile-ble -t triaxis/simplicity-studio:4-ble-2.13.0 .
+docker build -f Dockerfile-gecko -t triaxis/simplicity-studio:4-gecko-2.7.1 .
+docker build -f Dockerfile-ble -t triaxis/simplicity-studio:4-ble-2.13.1 .
