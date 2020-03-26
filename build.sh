2e4705c525920b093c85829febfe6e0fd2e6f472
Štefan Šimek <simek@triaxis.sk>
2020-03-26 13:37:57 +0100

    build.sh: build and push at once

--- build.sh
+++ build.sh
@@ -1,8 +1,23 @@
 #!/bin/sh
 
+set -e
+
 export DOCKER_BUILDKIT=1
+
+GECKO_VERSION=2.7.3
+GECKO_VERSION_BASE=${GECKO_VERSION%.*}
+
+BLE_VERSION=2.13.3
+BLE_VERSION_BASE=${BLE_VERSION%.*}
 
 docker build -f Dockerfile -t triaxis/simplicity-studio:4-base .
 docker build -f Dockerfile-gcc -t triaxis/simplicity-studio:4-gcc-7 .
-docker build -f Dockerfile-gecko -t triaxis/simplicity-studio:4-gecko-2.7.3 .
-docker build -f Dockerfile-ble -t triaxis/simplicity-studio:4-ble-2.13.3 .
+docker build -f Dockerfile-gecko -t triaxis/simplicity-studio:4-gecko-$GECKO_VERSION -t triaxis/simplicity-studio:4-gecko-$GECKO_VERSION_BASE .
+docker build -f Dockerfile-ble -t triaxis/simplicity-studio:4-ble-$BLE_VERSION -t triaxis/simplicity-studio:4-ble-$BLE_VERSION_BASE .
+
+docker image push triaxis/simplicity-studio:4-base
+docker image push triaxis/simplicity-studio:4-gcc-7
+docker image push triaxis/simplicity-studio:4-gecko-$GECKO_VERSION
+docker image push triaxis/simplicity-studio:4-gecko-$GECKO_VERSION_BASE
+docker image push triaxis/simplicity-studio:4-ble-$BLE_VERSION
+docker image push triaxis/simplicity-studio:4-ble-$BLE_VERSION_BASE
